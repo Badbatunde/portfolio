10fc4d5e8a97d9425449a5b9579bc5ae9f3b3d40
Badbatunde <hb.oyesile@gmail.com>
2023-03-27 05:27:02 +0100

    added a Math.ceil function for content

--- src/script.js
+++ src/script.js
@@ -1,27 +1,25 @@
-"https://kit.fontawesome.com/457a315592.js" 
+"https://kit.fontawesome.com/457a315592.js";
 
-const navbar = document.querySelector('nav');
+const navbar = document.querySelector("nav");
 window.onscroll = () => {
-    if (window.scrollY > 300) {
-        navbar.classList.add('nav-active');
-    } else {
-        navbar.classList.remove('nav-active');
-    }
+  if (window.scrollY > 300) {
+    navbar.classList.add("nav-active");
+  } else {
+    navbar.classList.remove("nav-active");
+  }
 };
 
 let header = document.querySelector(".nav-pages");
 let btns = header.getElementsByClassName("link");
 for (let i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function() {
-  let current = document.getElementsByClassName("active");
-  current[0].className = current[0].className.replace(" active", "");
-  this.className += " active";
+  btns[i].addEventListener("click", function () {
+    let current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
   });
 }
 
-const myStack = ["JavaScript", "Bootstrap"]
-myStack.push("React")
+let decimal = 9.1
+let getInt = Math.ceil(decimal)
 
-console.log(myStack) 
-// returns ["JavaScript", "Bootstrap", "React"]
-console.log(myStack.length) // returns 2
+console.log(getInt) // returns 10
