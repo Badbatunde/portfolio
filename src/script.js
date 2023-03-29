90ca406892514c8fbce7b2474647559b6eed976c
Badbatunde <hb.oyesile@gmail.com>
2023-03-29 07:00:35 +0100

    clean up the code and white spaces

--- src/script.js
+++ src/script.js
@@ -1,9 +1,33 @@
 "https://kit.fontawesome.com/457a315592.js";
 
 const navbar = document.querySelector("nav");
+const navPages = document.querySelectorAll(".nav-pages a")
+
 window.onscroll = () => {
-  if (window.scrollY > 300) {
+  if (window.scrollY > 0 && window.scrollY < 600) {
     navbar.classList.add("nav-active");
+    navPages[0].classList.add("active")
+    navPages[1].classList.remove("active")
+    navPages[2].classList.remove("active")
+    navPages[3].classList.remove("active")
+  } else if (window.scrollY > 600 && window.scrollY < 1400) {
+    navbar.classList.add("nav-active");
+    navPages[0].classList.remove("active")
+    navPages[1].classList.add("active")
+    navPages[2].classList.remove("active")
+    navPages[3].classList.remove("active")
+  } else if (window.scrollY > 1400 && window.scrollY < 2500){
+    navbar.classList.add("nav-active");
+    navPages[0].classList.remove("active")
+    navPages[1].classList.remove("active")
+    navPages[2].classList.add("active")
+    navPages[3].classList.remove("active")
+  } else if (window.scrollY > 2500){
+    navbar.classList.add("nav-active");
+    navPages[0].classList.remove("active")
+    navPages[1].classList.remove("active")
+    navPages[2].classList.remove("active")
+    navPages[3].classList.add("active")
   } else {
     navbar.classList.remove("nav-active");
   }
